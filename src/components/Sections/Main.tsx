37334f115b13858e3fafe21fdff2c9cea26cf1e5
@Hassanka <hassan.winners@gmail.com>
2023-05-08 17:25:13 +0100

    First test

--- src/components/Sections/Main.tsx
+++ src/components/Sections/Main.tsx
@@ -10,7 +10,7 @@ import AboutMe from "./AboutMe/index";
 import Hero from "./Hero/index";
 import Skills from "./Skills/index";
 import Projects from "./Projects/index";
-//experiance
+//experience
 import Experiance from "../Experiance/Experiance";
 //Context
 import { StateContext } from "../../context/CameraContext";
@@ -19,29 +19,31 @@ function Main() {
   //Context
   const { setCurrentSection } = useContext(StateContext);
   //Stats
-  const [activeSection, setActiveSection] = useState(1);
-  const containerRef = useRef(null);
+  const [activeSection, setActiveSection] = useState<number>(1);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleScroll = useCallback(() => {
-    const currentPosition = containerRef.current.scrollTop;
-    const sections = containerRef.current.children;
+    if (containerRef.current !== null) {
+      const currentPosition = containerRef.current.scrollTop;
+      const sections = containerRef.current.children;
 
-    for (let i = 0; i < sections.length; i++) {
-      const section = sections[i];
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.offsetHeight;
-      const sectionBottom = sectionTop + sectionHeight;
+      for (let i = 0; i < sections.length; i++) {
+        const section = sections[i] as HTMLElement;
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionBottom = sectionTop + sectionHeight;
 
-      if (currentPosition >= sectionTop && currentPosition < sectionBottom) {
-        setActiveSection(i + 1);
-        break;
+        if (currentPosition >= sectionTop && currentPosition < sectionBottom) {
+          setActiveSection(i + 1);
+          break;
+        }
       }
     }
   }, []);
 
   useEffect(() => {
     setCurrentSection(activeSection);
-  }, [activeSection]);
+  }, [activeSection, setCurrentSection]);
 
   return (
     <StyledMain>
