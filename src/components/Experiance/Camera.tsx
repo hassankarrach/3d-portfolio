0412908596cb7f948708fe9910ba6badf117e012
hassan karrach <zero@Zero.local>
2024-11-14 16:22:33 +0100

    almost there.

--- src/components/Experiance/Camera.tsx
+++ src/components/Experiance/Camera.tsx
@@ -1,49 +1,17 @@
-import React, { useState, useRef, useEffect, useContext } from "react";
+import { useRef, useEffect, useContext } from "react";
 import { PerspectiveCamera } from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
-import { gsap, Linear } from "gsap";
 import { StateContext } from "../../context/CameraContext";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { Vector3 } from "three";
-import { folder, useControls } from "leva";
-import { AnimationsData } from "../../data/AnimationsData";
 import { isMobile } from "../../utils/IsMobile";
+import { PerspectiveCamera as PerspectiveCameraType } from "three"; 
 
 function Camera() {
-  const { setCameraRef, CurrentSection } = useContext(StateContext);
-  const cameraRef = useRef();
+  const { setCameraRef } = useContext(StateContext);
+  const cameraRef = useRef<PerspectiveCameraType | null>(null);
   const isMobileDevice = isMobile();
 
   useEffect(() => {
     setCameraRef(cameraRef);
   }, []);
-
-  const { camera } = useThree();
-  // const [cameraDirection, setCameraDirection] = useState(new Vector3());
-
-  //CameraFollowsTheCursor
-  useFrame(({ mouse }) => {
-    // gsap.to(cameraRef.current.position, {
-    //   duration: 0.5,
-    //   delay: 0.05,
-    //   x: mouse.x * 0.05,
-    //   y: -mouse.y * 0.05,
-    //   ease: Linear.easeNone,
-    // });
-    // cameraRef.current.lookAt(AnimationsData[CurrentSection].CameraLookAt);
-    // switch (CurrentSection) {
-    //   case 1:
-    //     break;
-    //   case 2:
-    //     break;
-    //   case 3:
-    //     break;
-    //   case 4:
-    //     break;
-    //   default:
-    //     break;
-    // }
-  });
 
   return (
     <PerspectiveCamera
